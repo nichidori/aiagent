3c2e827a4984eeb70a98a1e30bfd10b1ef27d650
Arrafly Dori <arraflydori@gmail.com>
2026-01-10 14:33:52 +0700

    ch1 l7

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     
     client = genai.Client(api_key=api_key)
     
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
     
     response = client.models.generate_content(
         model="gemini-2.5-flash",
@@ -30,8 +34,10 @@ def main():
     prompt_token_count = response.usage_metadata.prompt_token_count
     response_token_count = response.usage_metadata.candidates_token_count
     
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {response_token_count}")
+    if args.verbose:
+        print(f"Prompt tokens: {prompt_token_count}")
+        print(f"Response tokens: {response_token_count}")
+
     print(response.text)
 
 if __name__ == "__main__":
