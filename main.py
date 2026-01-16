7e2fd264bc8806b4729581e9040c0d43234cd99e
Arrafly Dori <arraflydori@gmail.com>
2026-01-16 10:23:54 +0700

    ch4 l1

--- main.py
+++ main.py
@@ -26,6 +26,19 @@ def main():
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
 
+    finalized = None
+
+    for _ in range(20):
+        finalized = call_model(client, messages, verbose=args.verbose)
+        if finalized:
+            break
+
+    if not finalized:
+        print("Max iterations reached without finalizing the response.")
+        exit(1)
+
+
+def call_model(client, messages, verbose=False):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -35,38 +48,48 @@ def main():
         ),
     )
 
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
     if response.usage_metadata is None:
         raise RuntimeError("Response usage metadata not found")
 
     prompt_token_count = response.usage_metadata.prompt_token_count
     response_token_count = response.usage_metadata.candidates_token_count
 
-    if args.verbose:
+    if verbose:
         print(f"Prompt tokens: {prompt_token_count}")
         print(f"Response tokens: {response_token_count}")
 
     if response.function_calls:
-        function_results = []
-        
+        function_responses = []
+
         for function_call in response.function_calls:
-            function_call_result = call_function(function_call, verbose=args.verbose)
+            function_call_result = call_function(function_call, verbose=verbose)
 
             if not function_call_result.parts or len(function_call_result.parts) == 0:
                 raise RuntimeError("Function call result parts not found")
 
             if function_call_result.parts[0].function_response is None:
                 raise RuntimeError("Function response not found")
-            
+
             if function_call_result.parts[0].function_response.response is None:
                 raise RuntimeError("Function response content not found")
 
-            function_results.append(function_call_result.parts)    
-            
-            if args.verbose:
+            function_responses.append(function_call_result.parts[0])
+
+            if verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        messages.append(types.Content(role="user", parts=function_responses))
+
+        return False
 
     else:
         print(response.text)
+
+        return True
 
 
 if __name__ == "__main__":
